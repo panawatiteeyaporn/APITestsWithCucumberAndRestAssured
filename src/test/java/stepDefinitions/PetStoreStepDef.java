d24cab060cda13d4f462dd4dad6e78168f186cc1
DESKTOP-22G5TOV\pitee <piteey01@gmail.com>
2023-04-25 14:07:51 +0100

    Added test scenarios & refactor StepDef

--- src/test/java/stepDefinitions/PetStoreStepDef.java
+++ src/test/java/stepDefinitions/PetStoreStepDef.java
@@ -1,6 +1,9 @@
 package stepDefinitions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.After;
 import io.cucumber.java.BeforeAll;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -25,6 +28,14 @@ public class PetStoreStepDef {
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = "https://petstore.swagger.io/v2/pet";
+    }
+
+    @After
+    public void cleanUp() {
+        // Since the test is running on public database
+        // every Pet should be deleted after the test is complete to lower
+        // the change of testing against existing data on the next run
+        given().when().delete("/" + pet.getId()).then();
     }
 
     @Given("User created a Pet with the following data")
@@ -66,19 +77,11 @@ public class PetStoreStepDef {
 
     @Then("Pet with id {int} contains expected information")
     public void pet_with_id_contains_expected_information(Integer id) {
-
         var actualPet = given().when().get("/" + id).as(Pet.class);
-        assertThat(actualPet, samePropertyValuesAs(pet, "category", "tags"));
-
-        assertThat(actualPet.getCategory(), samePropertyValuesAs(pet.getCategory()));
-
-        for (int i = 0; i < pet.getTags().size(); i++) {
-            assertThat(actualPet.getTags().get(i), samePropertyValuesAs(pet.getTags().get(i)));
-        }
-
+        assertPetDetails(actualPet);
     }
 
-    @Then("User updated Pet data as follows")
+    @And("User updated Pet data as follows")
     public void user_updated_pet_data_as_follows(io.cucumber.datatable.DataTable dataTable) {
         List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
 
@@ -105,6 +108,60 @@ public class PetStoreStepDef {
         given().header(head).body(pet).put().then()
                 .assertThat().statusCode(200);
     }
+
+    @And("User updated Pet with form data")
+    public void user_updated_pet_with_form_data(io.cucumber.datatable.DataTable dataTable) {
+        List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
+
+        var head = new Header("Content-Type", "application/x-www-form-urlencoded");
+
+        String name = data.get(0).get("name");
+        String status = data.get(0).get("status");
+
+        // Update expected Pet object
+        pet.setName(name);
+        pet.setStatus(status);
+
+        given().header(head)
+                .formParam("name", name)
+                .formParam("status", status)
+                .when().post("/" + pet.getId()).then()
+                .assertThat().statusCode(200);
+    }
+
+    @Then("User can find Pet under status {string}")
+    public void user_can_find_pet_under_status(String status) {
+        var response = given().queryParam("status", status).when().get("/findByStatus")
+                .then().extract().jsonPath().get(String.format("find { it.id == %s }", pet.getId()));
+
+        // Using Jackson object mapper to convert json string to Pet object.
+        ObjectMapper objMapper = new ObjectMapper();
+        var actualPet = objMapper.convertValue(response, Pet.class);
+        assertPetDetails(actualPet);
+
+    }
+
+    @When("User deleted a Pet with id {int}")
+    public void user_deleted_a_pet_with_id(Integer id) {
+        given().when().delete("/" + id).then().assertThat().statusCode(200);
+    }
+
+    @Then("Pet with id {int} is not found")
+    public void pet_with_id_is_not_found(Integer id) {
+        given().when().delete("/" + id).then().assertThat().statusCode(404);
+    }
+
+    private void assertPetDetails(Pet actualPet) {
+
+        assertThat(actualPet, samePropertyValuesAs(pet, "category", "tags"));
+
+        assertThat(actualPet.getCategory(), samePropertyValuesAs(pet.getCategory()));
+
+        for (int i = 0; i < pet.getTags().size(); i++) {
+            assertThat(actualPet.getTags().get(i), samePropertyValuesAs(pet.getTags().get(i)));
+        }
+    }
+
 
 }
 
--- src/test/resources/features/PetStoreTests.feature
+++ src/test/resources/features/PetStoreTests.feature
@@ -1,5 +1,7 @@
 Feature: Pet API interaction
 
+  Todo - add Pet with multiple tags
+
   Scenario: User can add Pet to the store
     Given User created a Pet with the following data
       | id  | name | status    | urls      | category.id | category.name | tag.id | tag.name |
@@ -8,7 +10,7 @@ Feature: Pet API interaction
     Then The store contain Pet with id 212
     Then Pet with id 212 contains expected information
 
-  Scenario: User can update existing pet
+  Scenario: User can update existing Pet
     Given User created a Pet with the following data
       | id  | name | status    | urls      | category.id | category.name | tag.id | tag.name |
       | 111 | Luna | available | url1,url2 | 1           | dog           | 0      | myTag    |
@@ -19,3 +21,30 @@ Feature: Pet API interaction
       | Yuna | sold   | url3 | 1           | dog           | 1      | yourTag  |
     Then User updated Pet data on the store
     Then Pet with id 111 contains expected information
+
+  Scenario: User can update existing Pet with form data
+    Given User created a Pet with the following data
+      | id  | name | status    | urls | category.id | category.name | tag.id | tag.name |
+      | 515 | Yam  | available | url1 | 1           | dog           | 0      | myTag    |
+    When User added a Pet to the store
+    Then The store contain Pet with id 515
+    And User updated Pet with form data
+      | name | status |
+      | Yuna | sold   |
+    Then Pet with id 515 contains expected information
+
+  Scenario: User can find existing Pet by status
+    Given User created a Pet with the following data
+      | id  | name | status | urls | category.id | category.name | tag.id | tag.name |
+      | 313 | ted  | sold   | url1 | 1           | dog           | 0      | myTag    |
+    When User added a Pet to the store
+    Then User can find Pet under status "sold"
+
+  Scenario: User can delete existing Pet
+    Given User created a Pet with the following data
+      | id  | name | status    | urls | category.id | category.name | tag.id | tag.name |
+      | 141 | tim  | available | url1 | 1           | dog           | 0      | myTag    |
+    When User added a Pet to the store
+    Then The store contain Pet with id 141
+    When User deleted a Pet with id 141
+    Then Pet with id 141 is not found
